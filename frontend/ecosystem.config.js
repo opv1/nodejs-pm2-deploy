52a74b4bdc4c35ede73a9214998f22f8aef526cd
opv1 <pas7273@gmail.com>
2025-07-19 20:50:02 +0300

    fix

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      "post-deploy": "cd frontend && npm i && npm run build",
+      "post-deploy": "cd frontend && pwd && npm i && npm run build",
     },
   },
 };
